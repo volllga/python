7c0364d7339702524810242babdcbc9925d032d4
Volha Lukashevich <volllga@ya.ru>
2021-06-24 19:36:04 +0300

    New files to selenium_course_from_c_volga

---
+++ selenium_course_from_c_volga/test_fixture8.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+# Чтобы запустить тест с нужной маркировкой, нужно передать в командной строке параметр -m и нужную метку:
+# pytest -s -v -m smoke test_fixture8.py
+
+# Как же регистрировать метки?
+# Создайте файл pytest.ini в корневой директории вашего тестового проекта и добавьте в файл следующие строки:
+# [pytest]
+# markers =
+#     smoke: marker for smoke tests
+#     regression: marker for regression tests
